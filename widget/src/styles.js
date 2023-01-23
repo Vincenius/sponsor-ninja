a2f6185cb0e30da864b4dbb1b4482177674b37f0
Vincenius <vincent.will@vaitrade.de>
2023-01-23 15:07:51 +0100

    fix style error

--- widget/src/styles.js
+++ widget/src/styles.js
@@ -168,7 +168,7 @@ const styles = {
     boxShadow: '0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)',
     borderRadius: '5px',
     color: variables.textColor,
-    zIndex: '1000'
+    zIndex: '1000',
 
     '& h3': {
       color: variables.darkTextColor,
